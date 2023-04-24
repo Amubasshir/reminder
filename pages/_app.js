3859605c234d2e9baf4f83bbb3ecbc1c255cba43
Mubasshir Ahmed <lastmubasshir@gmail.com>
2023-04-24 15:08:41 +0600

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import 'componemt/styles/globals.css'
-
+import Head from 'next/head';
+import '/styles/globals.css';
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Reminder App</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
